869f23933300b2ff3a6aaa67f8758a4e8f03e11c
Jason Schmidt <jsn@visualplant.net>
2012-08-14 00:15:13 -0700

    check-in

---
+++ index.php
@@ -0,0 +1,459 @@
+<?php ?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8" />
+	<title>alias made</title>
+
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	
+	<!-- 1140px Grid styles for IE -->
+	<!--[if lte IE 9]><link rel="stylesheet" href="css/ie.css" type="text/css" media="screen" /><![endif]-->
+
+	<!-- The 1140px Grid - http://cssgrid.net/ -->
+	<link rel="stylesheet" href="css/1140.css" type="text/css" media="screen" />
+	
+	<!-- Your styles -->
+	<link rel="stylesheet" href="css/styles.css" type="text/css" media="screen" />
+	
+	<!--css3-mediaqueries-js - http://code.google.com/p/css3-mediaqueries-js/ - Enables media queries in some unsupported browsers-->
+	<script type="text/javascript" src="js/css3-mediaqueries.js"></script>
+
+</head>
+
+
+<body>
+
+<? include('includes/topbar.php') ?>
+
+<div class="container">
+	<div class="row">
+
+		<div class="twocol"></div>
+		
+		<div class="eightcol last" style="margin-top:80px;">
+		
+			<div class="name portfolio-head">
+				<p>Web . Mobile . Desktop</p>
+			</div>
+			
+		</div>
+		
+	</div>
+</div>
+
+<br style="clear:both;height:40px;" />
+
+<div class="container">
+	<div class="row">
+
+		<div class="twocol text-head project-name">Visualplant:</div>
+		
+		<div class="sevencol portfolio-thumb">
+			<p><a href="visualplant.php"><img src="images/visualplant/vp-home.jpg" alt=""></a></p>
+		</div>
+
+		<div class="threecol last portfolio-thumb">
+			<p><a href="visualplant.php"><img src="images/visualplant/uploader-solo.png" alt=""></a></p>
+		</div>
+		
+	</div>
+</div>
+
+<br style="clear:both;height:20px;" />
+
+<div class="container">
+	<div class="row">
+
+		<div class="twocol"></div>
+		
+		<div class="fivecol body-text">
+			<p><b>Visualplant</b> is a platform for businesses to upload, encode, curate and serve video on the Web.
+			Clients can accept user-generated video and use dozens of content management tools to moderate, package, 
+			publish and track viewership of their content.</p>
+		</div>
+		
+		<div class="fivecol last body-text" style="color:#999;">
+			<p>Roles: Founder and Product Designer
+			 
+			 <br /><br />
+			 <a href="visualplant.php" class="case-study-button">Case study</a>
+			 
+			</p>
+		</div>
+		
+	</div>
+</div>
+
+
+<div class="section-divider" style="margin-bottom:60px;">&nbsp;</div>
+
+
+
+<div class="container">
+	<div class="row">
+
+		<div class="twocol text-head project-name">Mokini:</div>
+		
+		<div class="sixcol portfolio-thumb">
+			<p><a href="mokini.php"><img src="images/mokini/mokini-start.png" alt=""></a></p>
+		</div>
+
+		<div class="fourcol last portfolio-thumb">
+			<p><a href="mokini.php"><img src="images/mokini/mokini-events-new.png" alt=""></a></p>
+		</div>
+		
+	</div>
+</div>
+
+<br style="clear:both;height:20px;" />
+
+<div class="container">
+	<div class="row">
+
+		<div class="twocol"></div>
+		
+		<div class="fivecol body-text">
+			<p><b>Mokini</b> is a Web 3.0 platform for exclusive interest groups to contribute their 
+			trusted information sources
+			and receive a prioritized feed of broadcasts from those sources &mdash; including blogs, 
+			Twitter, email groups and more.</p>
+		</div>
+		
+		<div class="fivecol last body-text" style="color:#999;">
+			<p>Roles: Co-founder and Product Designer
+			
+			<br /><br />
+			<a href="mokini.php" class="case-study-button">Case study</a>
+			
+			</p>
+			
+			
+			 
+		</div>
+		
+	</div>
+</div>
+
+<div class="section-divider" style="margin-bottom:60px;">&nbsp;</div>
+
+
+
+
+<div class="container">
+	<div class="row">
+
+		<div class="twocol text-head project-name">Traveal:</div>
+		
+		<div class="threecol portfolio-thumb">
+			<p><a href="traveal.php"><img src="images/traveal/Traveal-logo.png" alt=""></a>
+			<br /><br />
+			<a href="traveal.php"><img src="images/traveal/Traveal-Places.png" alt=""></a>
+			</p>
+		</div>
+		
+		<div class="sevencol last portfolio-thumb">
+			<p><a href="traveal.php"><img src="images/traveal/Traveal-Deals.png" alt=""></a></p>
+		</div>
+
+	</div>
+</div>
+
+<br style="clear:both;height:20px;" />
+
+<div class="container">
+	<div class="row">
+
+		<div class="twocol"></div>
+		
+		<div class="fivecol body-text">
+			<p><b>Traveal</b> is a Sabre-sponsored tablet and smartphone app that highlights daily hotel deals
+			for travel enthusiasts. Every day eight amazing deals from 
+			different cities are offered for purchase under an auction-style count down timer. Users can
+			book directly within the app.
+			</p>
+		</div>
+		
+		<div class="fivecol last body-text" style="color:#999;">
+			<p>Role: Product Designer
+			
+			<br /><br />
+			<a href="traveal.php" class="case-study-button">Case study</a>
+			
+			</p>
+
+		</div>
+		
+	</div>
+</div>
+
+
+<div class="section-divider" style="margin-bottom:60px;">&nbsp;</div>
+
+
+
+
+<div class="container">
+	<div class="row">
+
+		<div class="twocol text-head project-name">The Pulse:</div>
+		
+		<div class="tencol portfolio-thumb last">
+			<p><img src="images/sony/pulse1.png" alt=""></p>
+		</div>
+
+
+	</div>
+</div>
+
+<br style="clear:both;height:20px;" />
+
+<div class="container">
+	<div class="row">
+
+		<div class="twocol"></div>
+		
+		<div class="fivecol body-text">
+			<p><b>The Pulse</b> is a real-time data visualization commissioned by Sony Music to track
+			the digital distribution of music files to sales partners. It tracks 
+			over 100,000 transactions per hour to over 300 partners like Apple, Verizon and Spotify.
+			The app is Web-based and was built into a mult-screen physical installation at Sony Headquarters.
+			</p>
+		</div>
+		
+		<div class="fivecol last body-text" style="color:#999;">
+			<p>Role: Product Designer</p>
+		</div>
+		
+	</div>
+</div>
+
+<div class="section-divider" style="margin-bottom:60px;">&nbsp;</div>
+
+
+
+
+<div class="container">
+	<div class="row">
+
+		<div class="twocol text-head project-name">Advertising Age:</div>
+		
+		<div class="tencol portfolio-thumb last">
+			<p><img src="images/adage/adage-home.png" alt=""></p>
+		</div>
+
+
+	</div>
+</div>
+
+<br style="clear:both;height:20px;" />
+
+<div class="container">
+	<div class="row">
+
+		<div class="twocol"></div>
+		
+		<div class="fivecol body-text">
+			<p><b>Advertising Age</b> is a news and media publication owned by Crain Communications. For 80 years Ad Age
+			has covered advertising, marketing and media news for industry professionals, earning it the 
+			title "the bible of advertising."
+			</p>
+		</div>
+		
+		<div class="fivecol last body-text" style="color:#999;">
+			<p>Role: Director of Design and Product Management (2002 - 2007)</p>
+		</div>
+		
+	</div>
+</div>
+
+
+<div class="section-divider" style="margin-bottom:60px;">&nbsp;</div>
+
+
+
+
+<div class="container">
+	<div class="row">
+
+		<div class="twocol text-head project-name">Ruckus:</div>
+		
+		<div class="twocol portfolio-thumb">
+			<p><img src="images/ruckus/ruckus-logo.jpg" alt=""></p>
+		</div>
+		
+		<div class="eightcol portfolio-thumb last">
+			<p><img src="images/ruckus/ruckus.png" alt=""></p>
+		</div>
+
+
+	</div>
+</div>
+
+<br style="clear:both;height:20px;" />
+
+<div class="container">
+	<div class="row">
+
+		<div class="twocol"></div>
+		
+		<div class="fivecol body-text">
+			<p><b>Ruckus</b> was the legal version of Napster for participating colleges. The desktop application
+			allowed students to share music via a managed peer-to-peer network on campus. Costs for the music were
+			covered under tuition bills with the majority of revenues going to the music rights holders.
+			</p>
+		</div>
+		
+		<div class="fivecol last body-text" style="color:#999;">
+			<p>Role: Product Designer</p>
+		</div>
+		
+	</div>
+</div>
+
+
+
+<div class="section-divider" style="margin-bottom:60px;">&nbsp;</div>
+
+
+
+
+<div class="container">
+	<div class="row">
+
+		<div class="twocol text-head project-name">Upstairs:</div>
+		
+		<div class="eightcol portfolio-thumb">
+			<p><img src="images/upstairs/upstairsCover-new.jpg" alt=""></p>
+		</div>
+		
+		<div class="twocol portfolio-thumb last">
+			<p><img src="images/upstairs/title.jpg" alt=""></p>
+		</div>
+
+
+	</div>
+</div>
+
+<br style="clear:both;height:20px;" />
+
+<div class="container">
+	<div class="row">
+
+		<div class="twocol"></div>
+		
+		<div class="fivecol body-text">
+			<p><b>Upstairs</b> is a Web site devoted to architects, designers and their voyeurs. It highlights 
+			beautiful photography and video of homes and buildings from around the world. Powered by the 
+			Visualplant API, it allows users to create professional profiles and upload their own work.
+			</p>
+		</div>
+		
+		<div class="fivecol last body-text" style="color:#999;">
+			<p>Role: Founder / Product Designer</p>
+		</div>
+		
+	</div>
+</div>
+
+
+
+<div class="section-divider" style="margin-bottom:60px;">&nbsp;</div>
+
+
+
+
+<div class="container">
+	<div class="row">
+
+		<div class="twocol text-head project-name">Capsule:</div>
+		
+		<div class="twocol portfolio-thumb">
+			<p><img src="images/capsule/capsule-ipad-logo.png" alt=""></p>
+		</div>
+		
+		<div class="eightcol portfolio-thumb last">
+			<p><img src="images/capsule/capsule-ipad-browse.png" alt=""></p>
+		</div>
+
+
+	</div>
+</div>
+
+<br style="clear:both;height:20px;" />
+
+<div class="container">
+	<div class="row">
+
+		<div class="twocol"></div>
+		
+		<div class="fivecol body-text">
+			<p><b>Capsule</b> is a tablet app and marketplace for music artists to create and sell apps to their fans. 
+			The goal was to recreate the "album experience" using the form factor of the tablet
+			to create an immersive music experience.
+			
+			</p>
+		</div>
+		
+		<div class="fivecol last body-text" style="color:#999;">
+			<p>Role: Product Designer</p>
+		</div>
+		
+	</div>
+</div>
+
+
+
+<div class="section-divider" style="margin-bottom:60px;">&nbsp;</div>
+
+
+
+
+<div class="container">
+	<div class="row">
+
+		<div class="twocol text-head project-name">Talisman:</div>
+		
+		<div class="sixcol portfolio-thumb last">
+			<p><img src="images/talisman/talisman.png" alt=""></p>
+		</div>
+
+	</div>
+</div>
+
+<br style="clear:both;height:20px;" />
+
+<div class="container">
+	<div class="row">
+
+		<div class="twocol"></div>
+		
+		<div class="fivecol body-text">
+			<p><b>Talisman</b> is a real-world smartphone game where players plant and find geo pointers 
+			 that may include physical treasures. Users compete against each other 
+			 to check in at many locations and earn points toward badges.
+			</p>
+		</div>
+		
+		<div class="fivecol last body-text" style="color:#999;">
+			<p>Role: Logo / Icon Designer</p>
+		</div>
+		
+	</div>
+</div>
+
+
+
+
+
+
+<br style="clear:both;" /><br/><br />	
+
+<? include('includes/footer.php') ?>
+
+
+</body>
+
+</html>
